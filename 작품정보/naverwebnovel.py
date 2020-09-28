620f5d647056ee85440ab05955196ce3c9ee693b
hazelnutlemon <69499057+hazelnutlemon@users.noreply.github.com>
2020-09-29 02:55:26 +0900

    작품 정보 크롤러 - 2차 수정
    조아라 리디북스 : 한승주
    카카오페이지 문피아 : 김성종
    네이버웹소설 시리즈온 : 조예슬

--- 작품정보/naverwebnovel.py
+++ 작품정보/naverwebnovel.py
@@ -1,124 +1,409 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # 네이버 웹소설
-
+import time
+import bs4
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from selenium import webdriver
+from urllib import parse
+import time
 
-req = requests.get('https://novel.naver.com/webnovel/list.nhn?novelId=699567')
-source = req.content
-soup = BeautifulSoup(source, 'html.parser')
+n = 101
+Flag = True
+title_list = []
+total = 0
+pre_title=None
+while True:
+    if n == 101:
 
-################## 제목 ####################
-container = soup.find('h2')
-con = container.text
-print("제목: " + con)
+        i = 1
+        sam_url = "https://novel.naver.com/webnovel/genre.nhn?genre=101&page={}"
 
-################## 글, 그림 ####################
-f_wri_ill = soup.find('p', {'class', 'writer'})
-author = f_wri_ill.find('a', {'class', 'NPI=a:writer'})
-aut = author.text
-illustrator = f_wri_ill.find('a', {'class', 'NPI=a:illustrator'})
-ill = illustrator.text
-print("글: " + aut)
-print("그림: " + ill)
+        driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
+        driver.get(sam_url)
+        soup2 = bs4.BeautifulSoup(driver.page_source, "html.parser")
 
-################## 별점 ####################
-f_stargrade = soup.find('p', {'class', 'grade_area'})
-ff_stargrade = f_stargrade.find('em')
-stargrade = ff_stargrade.text
-print("별점: " + stargrade)
+        time.sleep(3)
+        comm = driver.find_element_by_css_selector("#wrap > div.ly_event_contest > div.contest_cont > a.lk_close")
+        comm.click()
+        time.sleep(2)
 
-############### 관심, 연재일, 장르 ##################
-info = soup.find('p', {'class', 'info_book'})
-f_like = info.find('span', {'id': 'concernCount'})
-like = f_like.text
+        Flag = True
+        while True:
+            url = sam_url.format(i)
+            req = requests.get(url)
+            source = req.content
+            bs_obj = BeautifulSoup(source, "html.parser")
+            #print(bs_obj)
 
-f_publish = info.find('span', {'class', 'publish'})
-publish = f_publish.text.replace("연재","")
+            f_container = bs_obj.find('ul', {'class', 'list_type1 v2 NE=a:lst_rom'})
+            title = f_container.find('li').find('a').attrs['title']
+            if pre_title == title:
+                print(n)
+                n = n + 1
+                break
+            pre_title=title
+            con = f_container.findAll('li')
 
-f_genre = info.find('span', {'class', 'genre'})
-genre = f_genre.text
+            for li in con:
+                title_list.append([])
+                f_title = li.find('a').attrs['title']
+                f_url = li.find('a').attrs['href']
+                title_list[total].append(f_title)
+                title_list[total].append("https://m.novel.naver.com/"+f_url)
+                title_list[total].append('네이버 웹소설')
+                print(f_title)
+                print(f_url)
+                total = total + 1
 
-print("관심: " + like)
-print("연재일: " + publish)
-print("장르: " + genre)
+            i = i + 1
 
-################## 연재 화수 ####################
-list_count = soup.find('span', {'class', 'total'})
-count1 = list_count.text.replace("(","")
-count = count1.replace(")", "")
-print("연재 화수: " + count)
+    elif n == 102:
+        i = 1
+        sam_url = "https://novel.naver.com/webnovel/genre.nhn?genre=102&page={}"
+        driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
+        driver.get(sam_url)
+        soup2 = bs4.BeautifulSoup(driver.page_source, "html.parser")
 
-################## 소개 ####################
-f_dsc = soup.find('p', {'class', 'dsc'})
-dsc = f_dsc.text
-print("소개: " + dsc)
+        time.sleep(3)
+        comm = driver.find_element_by_css_selector("#wrap > div.ly_event_contest > div.contest_cont > a.lk_close")
+        comm.click()
+        time.sleep(2)
+        Flag = True
+        while True:
+            url = sam_url.format(i)
+            req = requests.get(url)
+            source = req.content
+            bs_obj = BeautifulSoup(source, "html.parser")
+            #print(bs_obj)
+
+            #print(url)
+            f_container = bs_obj.find('ul', {'class', 'list_type1 v2 NE=a:lst_fan'})
+            #print(f_container)
+            title = f_container.find('li').find('a').attrs['title']
+            if pre_title == title:
+                print(n)
+                n = n + 1
+                break
+            pre_title = title
+            con = f_container.findAll('li')
+
+            for li in con:
+                title_list.append([])
+                f_title = li.find('a').attrs['title']
+                f_url = li.find('a').attrs['href']
+                title_list[total].append(f_title)
+                title_list[total].append("https://m.novel.naver.com/"+f_url)
+                title_list[total].append('네이버 웹소설')
+                print(f_title)
+                print(f_url)
+                total = total + 1
+
+            i = i + 1
+
+    elif n == 103:
+        i = 1
+        sam_url = "https://novel.naver.com/webnovel/genre.nhn?genre=103&page={}"
+        driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
+        driver.get(sam_url)
+        soup2 = bs4.BeautifulSoup(driver.page_source, "html.parser")
+
+        time.sleep(3)
+        comm = driver.find_element_by_css_selector("#wrap > div.ly_event_contest > div.contest_cont > a.lk_close")
+        comm.click()
+        time.sleep(2)
+        Flag = True
+        while True:
+            url = sam_url.format(i)
+            req = requests.get(url)
+            source = req.content
+            bs_obj = BeautifulSoup(source, "html.parser")
+
+            f_container = bs_obj.find('ul', {'class', 'list_type1 v2 NE=a:lst_hro'})
+            title = f_container.find('li').find('a').attrs['title']
+            if pre_title == title:
+                print(n)
+                n = n + 1
+                break
+            pre_title = title
+            con = f_container.findAll('li')
+
+            for li in con:
+                title_list.append([])
+                f_title = li.find('a').attrs['title']
+                f_url = li.find('a').attrs['href']
+                title_list[total].append(f_title)
+                title_list[total].append("https://m.novel.naver.com/"+f_url)
+                title_list[total].append('네이버 웹소설')
+                print(f_title)
+                print(f_url)
+                total = total + 1
+
+            i = i + 1
+
+    elif n == 104:
+        i = 1
+        sam_url = "https://novel.naver.com/webnovel/genre.nhn?genre=104&page={}"
+        driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
+        driver.get(sam_url)
+        soup2 = bs4.BeautifulSoup(driver.page_source, "html.parser")
+
+        time.sleep(3)
+        comm = driver.find_element_by_css_selector("#wrap > div.ly_event_contest > div.contest_cont > a.lk_close")
+        comm.click()
+        time.sleep(2)
+        while True:
+            url = sam_url.format(i)
+            req = requests.get(url)
+            source = req.content
+            bs_obj = BeautifulSoup(source, "html.parser")
+
+            f_container = bs_obj.find('ul', {'class', 'list_type1 v2 NE=a:lst_mth'})
+            title = f_container.find('li').find('a').attrs['title']
+            if pre_title == title:
+                print(n)
+                n = n + 1
+                break
+            pre_title = title
+            con = f_container.findAll('li')
+
+            for li in con:
+                title_list.append([])
+                f_title = li.find('a').attrs['title']
+                f_url = li.find('a').attrs['href']
+                title_list[total].append(f_title)
+                title_list[total].append("https://m.novel.naver.com/"+f_url)
+                title_list[total].append('네이버 웹소설')
+                print(f_title)
+                print(f_url)
+                total = total + 1
+
+            i = i + 1
+
+    elif n == 105:
+        i = 1
+        sam_url = "https://novel.naver.com/webnovel/genre.nhn?genre=105&page={}"
+        driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
+        driver.get(sam_url)
+        soup2 = bs4.BeautifulSoup(driver.page_source, "html.parser")
+
+        time.sleep(3)
+        comm = driver.find_element_by_css_selector("#wrap > div.ly_event_contest > div.contest_cont > a.lk_close")
+        comm.click()
+        time.sleep(2)
+        while True:
+            url = sam_url.format(i)
+            req = requests.get(url)
+            source = req.content
+            bs_obj = BeautifulSoup(source, "html.parser")
+
+            if 'list_type1 v2 NE=a:lst_his' in bs_obj:
+                f_container = bs_obj.find('ul', {'class', 'list_type1 v2 NE=a:lst_his'})
+                title = f_container.find('li').find('a').attrs['title']
+                if pre_title == title:
+                    print(n)
+                    n = n + 1
+                    break
+                pre_title = title
+                con = f_container.findAll('li')
+
+                for li in con:
+                    title_list.append([])
+                    f_title = li.find('a').attrs['title']
+                    f_url = li.find('a').attrs['href']
+                    title_list[total].append(f_title)
+                    title_list[total].append("https://m.novel.naver.com/"+f_url)
+                    title_list[total].append('네이버 웹소설')
+                    print(f_title)
+                    print(f_url)
+                    total = total + 1
+
+                i = i + 1
+            else:
+                n = n + 1
+                break
 
 
-################## 검색 ####################
-search = "https://novel.naver.com/search.nhn?keyword="
-search_for = input("검색: ")
-print("\n---------------------\n")
-web_search = search + search_for.replace(" ", "+")
 
-req = requests.get(web_search)
-source = req.text
-soup = BeautifulSoup(source, 'html.parser')
+    elif n == 106:
+        i = 1
+        sam_url = "https://novel.naver.com/webnovel/genre.nhn?genre=106&page={}"
+        driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
+        driver.get(sam_url)
+        soup2 = bs4.BeautifulSoup(driver.page_source, "html.parser")
 
-link = soup.find('ul', {'class', 'list_type2 v3'})
-href = link.find('a').attrs['href']
+        time.sleep(3)
+        comm = driver.find_element_by_css_selector("#wrap > div.ly_event_contest > div.contest_cont > a.lk_close")
+        comm.click()
+        time.sleep(2)
+        while True:
+            url = sam_url.format(i)
+            req = requests.get(url)
+            source = req.content
+            bs_obj = BeautifulSoup(source, "html.parser")
 
-novel_url = "https://novel.naver.com" + href
+            if 'list_type1 v2 NE=a:lst_lno' in bs_obj:
+                f_container = bs_obj.find('ul', {'class', 'list_type1 v2 NE=a:lst_lno'})
+                title = f_container.find('li').find('a').attrs['title']
+                if pre_title == title:
+                    print(n)
+                    n = n + 1
+                    break
+                pre_title = title
+                con = f_container.findAll('li')
 
-req = requests.get(novel_url)
-source = req.text
-soup = BeautifulSoup(source, 'html.parser')
+                for li in con:
+                    title_list.append([])
+                    f_title = li.find('a').attrs['title']
+                    f_url = li.find('a').attrs['href']
+                    title_list[total].append(f_title)
+                    title_list[total].append("https://m.novel.naver.com/" + f_url)
+                    title_list[total].append('네이버 웹소설')
+                    print(f_title)
+                    print(f_url)
+                    total = total + 1
 
-################## 제목 ####################
-container = soup.find('h2')
-con = container.text
-print("제목: " + con)
+                i = i + 1
+            else:
+                n = n + 1
+                break
 
-################## 글, 그림 ####################
-f_wri_ill = soup.find('p', {'class', 'writer'})
-author = f_wri_ill.find('a', {'class', 'NPI=a:writer'})
-aut = author.text
-illustrator = f_wri_ill.find('a', {'class', 'NPI=a:illustrator'})
-ill = illustrator.text
-print("글: " + aut)
-print("그림: " + ill)
 
-################## 별점 ####################
-f_stargrade = soup.find('p', {'class', 'grade_area'})
-ff_stargrade = f_stargrade.find('em')
-stargrade = ff_stargrade.text
-print("별점: " + stargrade)
+    elif n == 107:
+        print(n)
+        n = n + 1
 
-############### 관심, 연재일, 장르 ##################
-info = soup.find('p', {'class', 'info_book'})
-f_like = info.find('span', {'id': 'concernCount'})
-like = f_like.text
+    elif n == 108:
+        print(n)
+        n = n + 1
 
-f_publish = info.find('span', {'class', 'publish'})
-publish = f_publish.text.replace("연재","")
+    elif n == 109:
+        i = 1
+        sam_url = "https://novel.naver.com/webnovel/genre.nhn?genre=109&page={}"
+        driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
+        driver.get(sam_url)
+        soup2 = bs4.BeautifulSoup(driver.page_source, "html.parser")
 
-f_genre = info.find('span', {'class', 'genre'})
-genre = f_genre.text
+        time.sleep(3)
+        comm = driver.find_element_by_css_selector("#wrap > div.ly_event_contest > div.contest_cont > a.lk_close")
+        comm.click()
+        time.sleep(2)
+        while True:
+            url = sam_url.format(i)
+            req = requests.get(url)
+            source = req.content
+            bs_obj = BeautifulSoup(source, "html.parser")
 
-print("관심: " + like)
-print("연재일: " + publish)
-print("장르: " + genre)
+            f_container = bs_obj.find('ul', {'class', 'list_type1 v2 NE=a:lst_rof'})
+            title = f_container.find('li').find('a').attrs['title']
+            if pre_title == title:
+                print(n)
+                n = n + 1
+                break
+            pre_title = title
+            con = f_container.findAll('li')
 
-################## 연재 화수 ####################
-list_count = soup.find('span', {'class', 'total'})
-count1 = list_count.text.replace("(","")
-count = count1.replace(")", "")
-print("연재 화수: " + count)
+            for li in con:
+                title_list.append([])
+                f_title = li.find('a').attrs['title']
+                f_url = li.find('a').attrs['href']
+                title_list[total].append(f_title)
+                title_list[total].append("https://m.novel.naver.com/"+f_url)
+                title_list[total].append('네이버 웹소설')
+                print(f_title)
+                print(f_url)
+                total = total + 1
 
-################## 소개 ####################
-f_dsc = soup.find('p', {'class', 'dsc'})
-dsc = f_dsc.text
-print("소개: " + dsc)
+            i = i + 1
 
+    elif n == 110:
+        i = 1
+        sam_url = "https://novel.naver.com/webnovel/genre.nhn?genre=110&page={}"
+        driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
+        driver.get(sam_url)
+        soup2 = bs4.BeautifulSoup(driver.page_source, "html.parser")
+
+        time.sleep(3)
+        comm = driver.find_element_by_css_selector("#wrap > div.ly_event_contest > div.contest_cont > a.lk_close")
+        comm.click()
+        time.sleep(2)
+        while True:
+            url = sam_url.format(i)
+            req = requests.get(url)
+            source = req.content
+            bs_obj = BeautifulSoup(source, "html.parser")
+
+            f_container = bs_obj.find('ul', {'class', 'list_type1 v2 NE=a:lst_mof'})
+            title = f_container.find('li').find('a').attrs['title']
+            if pre_title==title:
+                print(n)
+                n = n + 1
+                break
+            pre_title=title
+            con = f_container.findAll('li')
+
+            for li in con:
+                title_list.append([])
+                f_title = li.find('a').attrs['title']
+                f_url = li.find('a').attrs['href']
+                title_list[total].append(f_title)
+                title_list[total].append("https://m.novel.naver.com/"+f_url)
+                title_list[total].append('네이버 웹소설')
+                print(f_title)
+                print(f_url)
+                total = total + 1
+
+            i = i+1
+
+    elif n == 111:
+        i = 1
+        sam_url = "https://novel.naver.com/webnovel/finish.nhn?page={}"
+        driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
+        driver.get(sam_url)
+        soup2 = bs4.BeautifulSoup(driver.page_source, "html.parser")
+
+        time.sleep(3)
+        comm = driver.find_element_by_css_selector("#wrap > div.ly_event_contest > div.contest_cont > a.lk_close")
+        comm.click()
+        time.sleep(2)
+        while True:
+            url = sam_url.format(i)
+            req = requests.get(url)
+            source = req.content
+            bs_obj = BeautifulSoup(source, "html.parser")
+
+            if 'list_type1 v2 NE=a:lst_end' in bs_obj:
+                f_container = bs_obj.find('ul', {'class', 'list_type1 v2 NE=a:lst_end'})
+                title = f_container.find('li').find('a').attrs['title']
+                if pre_title == title:
+                    print(n)
+                    n = n + 1
+                    break
+                pre_title = title
+                con = f_container.findAll('li')
+
+                for li in con:
+                    title_list.append([])
+                    f_title = li.find('a').attrs['title']
+                    f_url = li.find('a').attrs['href']
+                    title_list[total].append(f_title)
+                    title_list[total].append("https://m.novel.naver.com/" + f_url)
+                    title_list[total].append('네이버 웹소설')
+                    print(f_title)
+                    print(f_url)
+                    total = total + 1
+
+                i = i + 1
+            else:
+                n = n + 1
+                break
+
+    #n = n + 1
+
+    if n == 112:
+        Flag = False
+        break
+
+data = pd.DataFrame(title_list, columns=['작품명', '링크', '연재 플랫폼'])
+data.to_csv('naver_webnovel.csv', encoding='utf-8-sig', index=False)
+
+print("저장 완료.\n")
--- 작품정보/serieson.py
+++ 작품정보/serieson.py
@@ -1,143 +1,41 @@
+import time
+import bs4
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-req = requests.get('https://series.naver.com/novel/detail.nhn?productNo=3200031')
-source = req.text
-soup = BeautifulSoup(source, 'html.parser')
+n=1
+base_url= "https://series.naver.com/novel/categoryProductList.nhn?categoryTypeCode=all&page={}"
+Flag = True
 
-################## 제목, 화수 ####################
-f_container = soup.find('div', {'class', 'end_head'})
-container = f_container.find('h2')
-con = container.text.split("[")[0]
-con2 = con.split("-")[0]
-print("제목: " + con2)
-count = container.find('em')
-count1 = count.text.replace("- 총","")
-count2 = count1.replace("화/","")
-if '미완결' in count2:
-    count3 = count2.replace("미완결","")
-else:
-    count3 = count2.replace("완결","")
-print("화수: " + count3)
+while True:
+    url = base_url.format(n)
+    driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
+    driver.implicitly_wait(3)
+    driver.get(url)
+    #print("Page = " +str(n))
+    bs_obj = bs4.BeautifulSoup(driver.page_source, "html.parser")
 
-################## 저자 ####################
-box = soup.find(id='container')
-info_list = box.find('li', {'class', 'info_lst'})
-li = info_list.findAll('li')
-print("저자: " + li[0].find('a').text)
+    #for i in range(25)
+    f_container = bs_obj.find('div', {'class', 'lst_thum_wrap'})
+    container = f_container.findAll('h3')
 
-############# 그림 유무 판단, 출력 ##############
-if "그림" in li[1].find('span'):
-    n = 1
-else:
-    n = 0
-if n == 1:
-    print("그림: " + li[1].find('a').text)
+    for h3 in container:
+        f_title = h3.find('a').attrs['title']
+        #if '[독점]' in f_title:
+        #    title = f_title.replace("[독점]","")
+        #elif '[선공개]' in f_title:
+        #    title = f_title.replace("[선공개]","")
+        #elif '[단행본]' in f_title:
+        #    title = f_title.replace("[단행본]", "")
+        #elif '[개정판]' in f_title:
+        #    title = f_title.replace("[개정판]", "")
+        #elif '[외전]' in f_title:
+        #    title = f_title.replace("[외전]", "")
+        #elif '[무료연재]' in f_title:
+        #    title = f_title.replace("[무료연재]", "")
+        #else:
+        #    title = f_title
+        print(title)
 
-############# 장르, 출판사, 등급 ###############
-print("장르: " + li[1+n].find('a').text)
-print("출판사: " + li[2+n].find('a').text)
-print("등급: " + li[3+n].text.replace("등급 ",""))
-
-############# 완결 여부에 따른 프린트 ###############
-update = li[4+n].text.replace("업데이트 ","")
-if n == 1:
-    print("최근 업데이트:", update.replace("(완결)",""))
-else:
-    print("최근 업데이트:", update.replace("(미완결)", ""))
-if n == 1:
-    print("완결여부: 완결")
-else:
-    print("완결여부: 미완결")
-
-############### 별점 #################
-f_scorearea = soup.find('div', {'class', 'score_area'})
-ff_scorearea = f_scorearea.find('em')
-scorearea = ff_scorearea.text
-print("별점: " + scorearea)
-
-############### 소개 #################
-f_dsc = soup.find('div', {'class', '_synopsis'})
-print("소개: " + f_dsc.text)
-
-
-
-################## 검색 ####################
-search = "https://series.naver.com/search/search.nhn?t=all&fs=novel&q="
-search_for = input("검색: ")
-print("\n---------------------\n")
-web_search = search + search_for.replace(" ", "+")
-
-req = requests.get(web_search)
-source = req.text
-soup = BeautifulSoup(source, 'html.parser')
-
-link = soup.find('ul', {'class', 'lst_list'})
-href = link.find('a').attrs['href']
-
-novel_url = "https://series.naver.com/" + href
-
-req = requests.get(novel_url)
-source = req.text
-soup = BeautifulSoup(source, 'html.parser')
-
-################## 제목, 화수 ####################
-f_container = soup.find('div', {'class', 'end_head'})
-container = f_container.find('h2')
-con = container.text.split("[")[0]
-con2 = con.split("-")[0]
-print("제목: " + con2)
-count = container.find('em')
-count1 = count.text.replace("- 총","")
-count2 = count1.replace("화/","")
-if '미완결' in count2:
-    count3 = count2.replace("미완결","")
-else:
-    count3 = count2.replace("완결","")
-print("화수: " + count3)
-
-################## 저자 ####################
-box = soup.find(id='container')
-info_list = box.find('li', {'class', 'info_lst'})
-li = info_list.findAll('li')
-print("저자: " + li[0].find('a').text)
-
-############# 그림 유무 판단, 출력 ##############
-if "그림" in li[1].find('span'):
-    n = 1
-else:
-    n = 0
-if n == 1:
-    print("그림: " + li[1].find('a').text)
-
-############# 장르, 출판사, 등급 ###############
-print("장르: " + li[1+n].find('a').text)
-print("출판사: " + li[2+n].find('a').text)
-print("등급: " + li[3+n].text.replace("등급 ",""))
-
-############# 완결 여부에 따른 프린트 ###############
-update = li[4+n].text.replace("업데이트 ","")
-
-if '미완결' in count2:
-    a = 0
-else:
-    a = 1
-
-if a == 1:
-    print("최근 업데이트:", update.replace("(완결)",""))
-else:
-    print("최근 업데이트:", update.replace("(미완결)", ""))
-if a == 1:
-    print("완결여부: 완결")
-else:
-    print("완결여부: 미완결")
-
-############### 별점 #################
-f_scorearea = soup.find('div', {'class', 'score_area'})
-ff_scorearea = f_scorearea.find('em')
-scorearea = ff_scorearea.text
-print("별점: " + scorearea)
-
-############### 소개 #################
-f_dsc = soup.find('div', {'class', '_synopsis'})
-print("소개: " + f_dsc.text)
+    n = n+1
