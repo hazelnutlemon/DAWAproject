69e446ff43e3511c3b876ad3f831485399db16a4
hazelnutlemon <69499057+hazelnutlemon@users.noreply.github.com>
2020-10-08 19:18:15 +0900

    인스티즈 분석 데이터 수집 크롤러 - 4차 수정
    조아라 리디북스 트위터 인스타그램 : 한승주
    카카오페이지 문피아 네이버 블로그 다음 블로그 티스토리 : 김성종
    네이버웹소설 시리즈온 디씨인사이드 인스티즈 : 조예슬

--- 분석/코드/instiz.py
+++ 분석/코드/instiz.py
@@ -1,12 +1,3 @@
-# 코드 제작자 : 조예슬
-# 현재 버전 코드 수정자 : 김성종
-
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
-
 import bs4
 from bs4 import BeautifulSoup
 import requests
@@ -20,44 +11,142 @@ def hasxpath(xapth):
     except:
         return False
 
-url="https://www.instiz.net/m/index.htm?welcome=1"
-#https://www.instiz.net/name_enter/74027774
-driver = webdriver.Chrome('C:/chromedriver.exe')
+
+url = "https://www.instiz.net/m/index.htm?welcome=1"
+driver = webdriver.Chrome('C:/Users/luvub/Desktop/chromedriver_win32/chromedriver.exe')
 driver.implicitly_wait(5)
 driver.get(url)
 try:
-    driver.find_element_by_xpath.click('/html/body/div[8]/div/div[4]').click()
+    driver.find_element_by_xpath('/html/body/div[8]/div/div[4]').click()
 except:
     pass
 
-url="https://www.instiz.net/name_enter/74027774"
-driver.get(url)
-
-req = requests.get(url)
-source = req.content
-
-soup = BeautifulSoup(source, "html.parser")
-#print(soup)
-title = soup.head.find("meta", {"property": "og:title"}).get('content')
-content = soup.head.find("meta", {"name": "description"}).get('content')
-date = soup.head.find("meta", {"name": "description"}).get('content')
-date = date[0:13]
-count = soup.find('a', {'class', 'cmt'})
-con2 = soup.find('div', {'class', 'memo_content'})
-print(title, date)
-print(count.text)
-print(con2.text)
-try:
-    temp=driver.find_element_by_xpath('//*[@id="ajax_table"]/tbody/tr[1]/td[2]/div[2]').text.replace('•••답글','')
-    print(temp)
-    
-
-except:
-    pass
+tmp=input("작품제목 : ")
+n=1
+base_url= "https://www.instiz.net/popup_search.htm#gsc.tab=0&gsc.q=" + parse.quote(tmp) + "&gsc.page={}"
+Flag=True
+while True:
+    #작품의 링크
+    url=base_url.format(n)
+    driver.implicitly_wait(5)
+    driver.get(url)
+    print("Page = " + str(n))
+    bs_obj = bs4.BeautifulSoup(driver.page_source, "html.parser")
 
 
-# In[ ]:
+    base_xpath='//*[@id="___gcse_0"]/div/div/div/div[5]/div[2]/div[2]/div/div[1]/div[{}]/div[1]/div[2]/div[2]'
+
+    for i in range(10):
+        xpath=base_xpath.format(i+1)
+        try:
+            box_area=driver.find_element_by_xpath(xpath)
+        except:
+            Flag=False
+            break
+
+        baurl = box_area.text
+
+        test = 'https://'
+        test2 = 'www.'
+
+        if test in baurl:
+            #print(baurl)
+            driver.get(baurl)
+            req = requests.get(baurl)
+            source = req.content
+
+            soup = BeautifulSoup(source, "html.parser")
+            #print(soup)
+            title = soup.head.find("meta", {"property": "og:title"}).get('content')
+            content = soup.head.find("meta", {"name": "description"}).get('content')
+
+            if "대한민국 최대의 연예·오락 커뮤니티" in content:
+                continue
+
+            date = soup.head.find("meta", {"name": "description"}).get('content')
+            date = date[0:13]
+            count = soup.find('a', {'class', 'cmt'})
+            con2 = soup.find('div', {'class', 'memo_content'})
+            for tag in con2.find_all(['span']):
+                tag.replace_with('')
+            print(title, date)
+            print(count.text)
+            print(con2)
+
+            try:
+                temp = driver.find_element_by_xpath('//*[@id="ajax_table"]/tbody/tr[1]/td[2]/div[2]').text.replace(
+                    '•••답글', '')
+                print(temp)
+            except:
+                pass
 
 
 
+        elif test2 in baurl:
+            m_url = "https://" + baurl
+            driver.get(m_url)
+            req = requests.get(m_url)
+            source = req.content
 
+            soup = BeautifulSoup(source, "html.parser")
+
+            title = soup.head.find("meta", {"property": "og:title"}).get('content')
+            content = soup.head.find("meta", {"name": "description"}).get('content')
+
+            if "대한민국 최대의 연예·오락 커뮤니티" in content:
+                continue
+
+            date = soup.head.find("meta", {"name": "description"}).get('content')
+            date = date[0:13]
+            count = soup.find('a', {'class', 'cmt'})
+            con2 = soup.find('div', {'class', 'memo_content'})
+            for tag in con2.find_all(['span']):
+                tag.replace_with('')
+            print(title, date)
+            print(count.text)
+            print(con2)
+
+            try:
+                temp = driver.find_element_by_xpath('//*[@id="ajax_table"]/tbody/tr[1]/td[2]/div[2]').text.replace(
+                    '•••답글', '')
+                print(temp)
+            except:
+                pass
+
+
+
+        else:
+            mm_url = "https://www." + baurl
+            driver.get(mm_url)
+            req = requests.get(mm_url)
+            source = req.content
+
+            soup = BeautifulSoup(source, "html.parser")
+            print(soup)
+            title = soup.head.find("meta", {"property": "og:title"}).get('content')
+            content = soup.head.find("meta", {"name": "description"}).get('content')
+
+            if "대한민국 최대의 연예·오락 커뮤니티" in content:
+                continue
+
+            date = soup.head.find("meta", {"name": "description"}).get('content')
+            date = date[0:13]
+            count = soup.find('a', {'class', 'cmt'})
+            con2 = soup.find('div', {'class', 'memo_content'})
+            for tag in con2.find_all(['span']):
+                tag.replace_with('')
+            print(title, date)
+            print(count.text)
+            print(con2)
+
+            try:
+                temp = driver.find_element_by_xpath('//*[@id="ajax_table"]/tbody/tr[1]/td[2]/div[2]').text.replace(
+                    '•••답글', '')
+                print(temp)
+            except:
+                pass
+
+    if(Flag==False):
+        print("프로그램을 종료합니다")
+        break
+    n=n+1
